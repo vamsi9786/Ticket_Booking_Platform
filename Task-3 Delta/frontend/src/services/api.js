c67ad4cb3527ed3a8ab6c88e67409fff79609d85
vamsi9786 <vamsi6319@gmail.com>
2026-06-28 18:07:25 +0530

    Update api.js

--- Task-3 Delta/frontend/src/services/api.js
+++ Task-3 Delta/frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API=axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
 });
 
 API.interceptors.request.use(config => {

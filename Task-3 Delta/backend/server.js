99d9a3a518afb443abfa5c5ee85d91e58a60d794
vamsi9786 <vamsi6319@gmail.com>
2026-06-28 18:38:34 +0530

    Update server.js

--- Task-3 Delta/backend/server.js
+++ Task-3 Delta/backend/server.js
@@ -14,8 +14,8 @@ const adminRoutes = require('./routes/adminRoutes');
 dotenv.config();
 
 const app = express();
-//app.use(cors({origin: 'http://localhost:3000', credentials: true}));
-app.use(cors());
+app.use(cors({origin: [ 'http://localhost:3000', 'https://ticket-booking-platform-frontend-jet.vercel.app'], credentials: true}));
+//app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads/avatars', express.static('uploads/avatars'));

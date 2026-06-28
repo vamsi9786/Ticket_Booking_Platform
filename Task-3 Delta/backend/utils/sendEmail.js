0d7e37cd051cb502c42015b77beda73e99581c2a
vamsi9786 <vamsi6319@gmail.com>
2026-06-28 19:11:11 +0530

    Update sendEmail.js

--- Task-3 Delta/backend/utils/sendEmail.js
+++ Task-3 Delta/backend/utils/sendEmail.js
@@ -1,20 +1,26 @@
 const nodemailer=require('nodemailer');
 
 const sendEmail=async(to,subject,text)=>{
-    const transporter=nodemailer.createTransport({
-        service:'Gmail',
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS
-        }
-    });
-
-    await transporter.sendMail({
-        from:process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-    });
+    try(){
+        const transporter=nodemailer.createTransport({
+            service:'Gmail',
+            auth:{
+                user:process.env.EMAIL_USER,
+                pass:process.env.EMAIL_PASS
+            }
+        });
+    
+        await transporter.sendMail({
+            from:process.env.EMAIL_USER,
+            to,
+            subject,
+            text,
+        });
+        console.log("Email sent successfully");
+    } catch(err){
+        console.error("Email Failed: ",err.message);
+        return;
+    }
 };
 
 module.exports=sendEmail;
